f7986d736b38c3d269543a49538d1aa0e2ef901c
TTVSouthern <69335687+TTVSouthern@users.noreply.github.com>
2021-06-09 13:19:52 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -7,5 +7,12 @@ mobile: false, //change this to true or false if you want to
 token: "TOKEN", //Change this and go to discord.dev to get your token
 prefix: "!" //You can change this to whatever you want
 })
+
 bot.onMessage() //makes your bot be able to use commands or say stuff
 bot.loadCommands(`/commands`/) //Please create a folder called commands to use this
+
+bot.status({
+  text: "Change Me",
+  type: "WATCHING", //If you change this to STREAMING add : url: "twitch.tv/yourusername"
+  status: "idle" //offline + online + idle + dnd
+})
